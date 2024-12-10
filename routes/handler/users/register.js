9ac94ff724dccc87614da9956c086ea2228f3842
Yoedi Arianto <yoediarianto@gmail.com>
2024-12-10 17:01:58 +0700

    Create api register user

---
+++ routes/handler/users/register.js
@@ -0,0 +1,52 @@
+const bcrypt = require('bcrypt');
+const Validator = require('fastest-validator');
+const v = new Validator();
+const { User } = require('../../../models/User');
+
+module.exports = async (req, resp) => {
+  const schema = {
+    name: "string|empty:false",
+    email: "string|empty:false",
+    password: "string|min:6",
+    profession: "string|optional",
+  }
+
+  const validate = v.validate(req.body, schema);
+
+  if (validate.length) {
+    return resp.status(400).json({
+      status: 'error',
+      message: validate,
+    });
+  }
+
+  const user = await User.findOne({
+    where: { email: req.body.email }
+  });
+
+  if (user) {
+    return resp.status(409).json({
+      status: 'error',
+      message: 'email already exist.',
+    });
+  }
+
+  const password = await bcrypt.hash(req.body.password, 10);
+
+  const data = {
+    password,
+    name: req.body.name,
+    email: req.body.email,
+    profession: req.body.profession,
+    role: 'student',
+  }
+
+  const createUser = await User.create(data);
+
+  return resp.json({
+    status: 'success',
+    data: {
+      id: createUser.id
+    },
+  });
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 const express = require('express');
+const userHandler = require('./handler/users');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/register', userHandler.register);
 
 module.exports = router;
