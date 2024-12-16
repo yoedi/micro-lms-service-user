7ed116a258da8d7f7dc34edfe869afc274963a17
Yoedi Arianto <yoediarianto@gmail.com>
2024-12-16 16:57:46 +0700

    Add filter in api get all users

--- routes/handler/users/getUsers.js
+++ routes/handler/users/getUsers.js
@@ -2,9 +2,19 @@ const { User } = require('../../../models');
 
 module.exports = async (req, resp) => {
 
-  const users = await User.findAll({
+  const userIds = req.query.user_ids || [];
+
+  const sqlOptions = {
     attributes: ['id', 'name', 'email', 'role', 'profession', 'avatar']
-  });
+  }
+
+  if (userIds.length) {
+    sqlOptions.where = {
+      id: userIds
+    }
+  }
+
+  const users = await User.findAll(sqlOptions);
 
   return resp.json({
     status: 'success',
