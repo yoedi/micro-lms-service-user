f5f0912af15e54e485d70c9fbba9e6a4bb307b29
Yoedi Arianto <yoediarianto@gmail.com>
2024-12-16 16:56:12 +0700

    Create api get all users

--- routes/handler/users/getUsers.js
+++ routes/handler/users/getUsers.js
@@ -2,19 +2,9 @@ const { User } = require('../../../models');
 
 module.exports = async (req, resp) => {
 
-  const userIds = req.query.user_ids || [];
-
-  const sqlOptions = {
+  const users = await User.findAll({
     attributes: ['id', 'name', 'email', 'role', 'profession', 'avatar']
-  }
-
-  if (userIds.length) {
-    sqlOptions.where = {
-      id: userIds
-    }
-  }
-
-  const users = await User.findAll(sqlOptions);
+  });
 
   return resp.json({
     status: 'success',
